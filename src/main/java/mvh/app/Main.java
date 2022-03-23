5577e2a2c9494ee69504528a35d2b8f8d6dc5f9f
Sergey <sergeychits20@gmail.com>
2022-03-22 20:37:12 -0600

    added reader, and title

--- src/main/java/mvh/app/Main.java
+++ src/main/java/mvh/app/Main.java
@@ -3,9 +3,12 @@ package mvh.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Random;
 
 public class Main extends Application {
@@ -21,12 +24,26 @@ public class Main extends Application {
         launch(args);
     }
 
+    public static void setWindowTitle(Stage stage){
+        stage.setTitle("Monsters VS Heroes V1.0");
+    }
+
+    public static File getFile(Stage stage){
+        FileChooser myFileChooser = new FileChooser();
+        File selectedFile = myFileChooser.showOpenDialog(stage);
+        return selectedFile;
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         //Students edit here to set up the scene
+        File worldFile = getFile(stage);
+        //World world = Reader.loadWorld(worldFile);
         stage.setScene(scene);
+        setWindowTitle(stage);
         stage.show();
+
     }
 }
--- src/main/java/mvh/util/Reader.java
+++ src/main/java/mvh/util/Reader.java
@@ -5,8 +5,7 @@ import mvh.world.Hero;
 import mvh.world.Monster;
 import mvh.world.World;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -27,6 +26,91 @@ public final class Reader {
      * @return A World created from the world file
      */
     public static World loadWorld(File fileWorld) {
+        try {
+            // create a file reader
+            Scanner fileReader = new Scanner(fileWorld);
+
+            // read through and assign number of rows found in first line
+            String firstLine = fileReader.nextLine();
+            int rowCount = Integer.parseInt(firstLine);
+
+            // read through and assign number of columns found in second line
+            String secondLine = fileReader.nextLine();
+            int columnCount = Integer.parseInt(secondLine);
+
+            // create new world object
+            World world = new World(rowCount, columnCount);
+
+            // calculates number of lines
+            int lineNum = rowCount * columnCount;
+
+            // loops through while line number is greater than zero
+            while (lineNum > 0){
+
+                // reads onto the next line from the file
+                String line = fileReader.nextLine();
+
+                // splits the line using "," delimiter
+                String[] lineObjects = line.split(",");
+
+                // find length of line to determine the number of elements in the line
+                int numOfElementsInLine = lineObjects.length;
+
+                // checks if Hero or Monster is on tile
+                if (numOfElementsInLine > 2) {
+
+                    // assigns variables which are similar between Monster and Hero
+                    int row = Integer.parseInt(lineObjects[0]);
+                    int column = Integer.parseInt(lineObjects[1]);
+                    String type = lineObjects[2];
+                    char symbol = lineObjects[3].charAt(0);
+                    int health = Integer.parseInt(lineObjects[4]);
+
+                    // determine if Monster is on the tile
+                    if (type.equals("MONSTER")) {
+                        // assigns weaponType for the Monster
+                        char weaponTypeChar = lineObjects[5].charAt(0);
+                        WeaponType weaponType = WeaponType.getWeaponType(weaponTypeChar);
+                        // creates new object Monster
+                        Monster monster = new Monster(health, symbol, weaponType);
+                        // adds entity to the world object at specific row and column
+                        world.addEntity(row, column, monster);
+
+                    }
+
+                    // determines if Hero is on the tile
+                    if (type.equals("HERO")) {
+                        // assigns weapon and armor strength for the Hero
+                        int weaponStrength = Integer.parseInt(lineObjects[5]);
+                        int armorStrength = Integer.parseInt(lineObjects[6]);
+                        // creates new object Hero
+                        Hero hero = new Hero(health, symbol, weaponStrength, armorStrength);
+                        // adds entity to the world object at specific row and column
+                        world.addEntity(row, column, hero);
+                    }
+                }
+                // subtract one from the lineNum
+                lineNum--;
+
+            }
+
+            // returns world object with Hero and Monster entities (if applicable)
+            return world;
+
+            // bring up error if file was not found
+        } catch (FileNotFoundException error) {
+            System.out.println("File was not found.");
+            error.printStackTrace();
+            System.exit(1);
+            // catch general error
+        } catch (Exception error){
+            System.out.println("Unexpected error happened while loading world.");
+            error.printStackTrace();
+            System.exit(1);
+        }
+        // default value for world
         return null;
     }
 }
+
+
